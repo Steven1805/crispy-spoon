bfd244216cb178b49498a65acd785d75bb81537e
Steven1805 <99425144+Steven1805@users.noreply.github.com>
2022-04-01 17:04:57 +0300

    Update 4-generate-unordered-array.js

--- 4-generate-unordered-array.js
+++ 4-generate-unordered-array.js
@@ -1,15 +1,18 @@
 const { arrayContain } = require('./3-array-contain');
 
 function generateUnorderedArray(size) {
-  let array = [];
-  for (let i = 0; i < size; i++) {
-    let number = Math.floor(Math.random() * 100);
-    while (arrayContain(array, number)) {
-      number = Math.floor(Math.random() * 100);
+  let array = []; //1
+  for (let i = 0; i < size; i++) { //5
+    let number = Math.floor(Math.random() * 100); //5 lignes 6 à 11 : 11n
+    while (arrayContain(array, number)) { //3
+      number = Math.floor(Math.random() * 100); //5
     }
-    array.push(number);
+    array.push(number); //3
   }
-  return array;
+  return array; //1
 }
 
 module.exports = { generateUnorderedArray };
+
+// 1 + 5 + 11n + 1 = 7 + 11n = O(7 + 11n) = O(n)
+//complexité linéaire
