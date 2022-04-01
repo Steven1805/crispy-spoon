71d4e56ebbb4126c51f04fe33f3daedd58f712a0
Steven1805 <99425144+Steven1805@users.noreply.github.com>
2022-04-01 16:55:27 +0300

    Update 1-slice.js

--- 1-slice.js
+++ 1-slice.js
@@ -1,9 +1,12 @@
-function slice(array, start, end) {
-  let slicedArray = [];
-  for (let i = start; i < end; i++) {
-    slicedArray.push(array[i]);
+function slice(array, start, end) { 
+  let slicedArray = []; //1
+  for (let i = start; i < end; i++) { //6
+    slicedArray.push(array[i]); //3 lignes 4 à 5 : 3n
   }
-  return slicedArray;
+  return slicedArray; //1
 }
 
 module.exports = { slice };
+
+//1 + 6 + 3n  + 1  = 8 + 3n donc O(8 + 3n) = O(n)
+//Complexité linéaire
