6980476adf6d99402ba81caef8661687518ddc65
Steven1805 <99425144+Steven1805@users.noreply.github.com>
2022-04-01 17:05:32 +0300

    Update 6-main.js

--- 6-main.js
+++ 6-main.js
@@ -3,15 +3,17 @@ const { merge }= require('./2-merge');
 const { generateUnorderedArray } = require('./4-generate-unordered-array');
 
 function sort(array) {
-  if (array.length <= 1) {
-    return array;
+  if (array.length <= 1) { //2
+    return array; //1
   }
-  const middle = Math.floor(array.length / 2);
-  const left = slice(array, 0, middle);
-  const right = slice(array, middle, array.length);
-  return merge(sort(left), sort(right));
+  const middle = Math.floor(array.length / 2); //4
+  const left = slice(array, 0, middle); // n
+  const right = slice(array, middle, array.length); //n
+  return merge(sort(left), sort(right)); //5
 }
 
-const array = generateUnorderedArray(10);
-console.log(array);
-console.log(sort(array));
+const array = generateUnorderedArray(10); //2
+console.log(array); //2
+console.log(sort(array)); //3
+//2 + 1 + 4 + n + n + 5 + 2 + 2 + 3 = 19 + 2n = O(19 + 2n)  = O(n)
+//complexité linéaire
